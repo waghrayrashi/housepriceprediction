a11165c7b22493c45258cad1456a2dc01c6fbb90
waghrayrashi <61018806+waghrayrashi@users.noreply.github.com>
2020-08-10 22:02:57 -0400

    x

--- app.py
+++ app.py
@@ -18,6 +18,25 @@ from flask import Flask, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 ##########################################
+# Database Connection Set up
+##########################################
+# Setup SQLAlchemy with flask
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '')
+# Create database and pass in the app 
+db = SQLAlchemy(app)
+
+# Create engine
+conn_str = "postgres://rilcwxqbdcttnx:6fbe02be55d33cf132b076fcec9f961162a0eb3ffc67aff912d8aedeabfcfe5e@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d3c3p19kp6bchj"
+engine = create_engine(conn_str)
+
+# Reflect the database 
+Base = automap_base()
+
+# Reflect the tables into SQLAlchemy classes
+Base.prepare(engine, reflect=True)
+allclasses = Base.classes.keys()
+
+##########################################
 # Flask Set up
 ##########################################
 app = Flask(__name__)
@@ -32,6 +51,40 @@ app = Flask(__name__)
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+#######################################################
+# Render the database table 1 BDR
+#######################################################
+# @app.route("/nc1bdr")
+# def nc1bdr():
+#     """Query the database for 1 BDR houses"""
+
+#     # Save references to each table in database 
+#     Propertytypes = Base.classes.propertytypes
+
+#     # Create session
+#     session = Session(bind=engine)
+    
+#     # Query the table for columns of interest
+#     propertytypes = session.query(Propertytypes).all()
+    
+#     # Create an empty list
+#     property_types = []
+    
+#     # Loop through each row and create dictionaries for each column   
+#     for listing in propertytypes:
+#         ptypes_dict = {
+#             "propertytype": listing.propertytype,
+#             "listings": listing.listings
+#             # "percent": listing.percent
+#             }
+        
+#         # Append all columns to the object
+#         property_types.append(ptypes_dict)
+    
+#     session.close()
+#     # Return the json
+#     return jsonify(property_types)
 
 #######################################################
 # Render the Dashboard HTML 
@@ -62,7 +115,7 @@ def acknowledgements():
 #######################################################
 @app.route("/analysis")
 def analysis():
-    """Return the analysis page."""
+    """Return the exploratory analysis page."""
     return render_template("rw_analysis.html")
 
 ########################################################
